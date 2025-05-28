9b37c4280721857ff4d1fcf3f28285e3b9e0d4d0
Gustavo Seidi Hono <gustavo.hono@fatec.sp.gov.br>
2025-05-28 16:09:18 -0300

    Tabelas

---
+++ Service/tabela_onibus.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conexao = sqlite3.connect("Rodoviaria.db")
+
+cursor = conexao.cursor()
+
+cursor.execute(
+    """
+        CREATE TABLE onibus (
+        id INTEGER PRIMARY KEY NOT NULL,
+        origem TEXT NOT NULL,
+        placa TEXT NOT NULL,
+        nome_locadoura TEXT NOT NULL,
+        qtn_assento INTEGER NOT NULL
+        );
+"""
+)
+
+cursor.close()
+print("Tabela Onibus funcionando")
