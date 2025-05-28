29388f29c27ed202d8515228f72afbe13cac8b50
Luis-creat <luisotaviojassi@gmail.com>
2025-05-28 14:51:48 -0300

    Classe dos clientes

--- Model/Cliente.py
+++ Model/Cliente.py
@@ -4,3 +4,9 @@ class Cliente(ABC):
     def __init__(self, codigo, nome):
         self.codigo = codigo
         self.nome = codigo
+
+    def get_codigo(self):
+        return self.codigo
+    
+    def set_codigo(self, codigo):
+        self.codigo = codigo
