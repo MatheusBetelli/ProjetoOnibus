c01fe6a10474a204f8f5702a348a1288b92eddde
Gustavo Seidi Hono <gustavo.hono@fatec.sp.gov.br>
2025-06-10 23:17:40 -0300

    Adicionando index para aceleração

--- Service/tabela_relatorio.py
+++ Service/tabela_relatorio.py
@@ -16,7 +16,16 @@ def gerar_relatorio(tipo):
         conexao.close()
         return df
     
-    # elif tipo == "Vendas "
+    elif tipo == "Vendas Com Clientes e Reserva":
+        query = """
+            SELECT v.id, v.preco, v.assento, v.id_onibus, c.nome AS cliente_nome,  o.placa AS onibus_placa
+                FROM venda v
+                JOIN cliente c ON v.id_cliente = c.id
+                JOIN onibus o ON v.id_onibus = o.id
+        """
+        df = pd.read_sql_query(query, conexao)
+        conexao.close()
+        return df
                        
         
     else:
